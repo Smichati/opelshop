c9e39e778fb8eafb82ef4e314e140ae23abe83e2
Smichati <mmoping620@gmail.com>
2020-04-29 19:04:57 +0200

    Problem resolved

--- src/pl/opelshop/service/MenuService.java
+++ src/pl/opelshop/service/MenuService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class MenuService {
 
     CarOperationsService carOperationsService = new CarOperationsService();
@@ -24,7 +23,6 @@ public class MenuService {
         choiceSelector(scanner.nextInt());
         showMainMenu();
     }
-
 
     private void choiceSelector(int choiceNumber) {
         switch (choiceNumber) {
